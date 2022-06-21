4f68fab5c7cd05b50113f8948181d762a4dd9b8b
slalom <fhakwpak6@naver.com>
2022-06-21 10:05:09 +0900

    edit

--- Day2/hello.js
+++ Day2/hello.js
@@ -3,5 +3,6 @@ let colors = require('colors');
 console.log(colors.rainbow('HELLO NodeJS!!!'));
 console.log(colors.green('HELLO NodeJS!!!'));
 console.log(colors.red('Hello NodeJS!!!'));
+console.log(colors.red('Hello NodeJS!!!'));
 let a = 5;
 console.log(a);

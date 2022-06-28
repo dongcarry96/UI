6c9d0c5505f47ee8af9c93747759c7bfe53c5871
slalom <fhakwpak6@naver.com>
2022-06-29 08:57:33 +0900

    20220628

--- js-day3/array.js
+++ js-day3/array.js
@@ -20,3 +20,15 @@ console.log(array);
 const deptnos = [10,20,30];
 array = Array.from(deptnos);
 console.log(array)
+
+array = Array.from({
+  0:10,
+  1:'개발부',
+  2:'서울',
+})
+console.log(array);
+
+// 자바에서 배열은 동일한 메모리 크기를 가지며, 연속적으로 이어져 있는데 반해서
+// 자바스크립트의 배열은 반드시 연속적이 아닐 수 있으며 오브젝트와 더 유사함
+// 자바스크립트에서 배열은 자바의 뱌열을 흉내낸 특수한 객체이다.
+// 단점을 보완하기 위해서 타입배열을 따로 제공하고 있음
